957eea802340a5b90cf345172b364394694219d2
NafisMahmudAyon <nafismahmudayon@gmail.com>
2025-01-10 10:50:46 +0600

    update html

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
+'use client';
+import { Button } from "aspect-ui";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter()
   return (
-    <main className="flex">
-      <div className="flex-1 text-primary-600 font-bold">fdfdf</div>
+    <main className="text-center">
+      <div className="text-display1 text-center mt-10 text-primary-600 font-bold">Accordion Builder</div>
+      <Button className="mx-auto mt-10 px-10" onClick={() => { router.push('/dashboard') }}>Get Started</Button>
     </main>
   );
 }
